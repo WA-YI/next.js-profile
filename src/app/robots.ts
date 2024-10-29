aac60346208ebd6df913f81ef5aef8f98b62683d
wayi <abc980514@qq.com>
2024-10-30 01:19:28 +0800

    Add export script and update next.config.js

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-    },
-    sitemap: `${siteConfig.metadata.url}/sitemap.xml`,
+    }
   };
 }
